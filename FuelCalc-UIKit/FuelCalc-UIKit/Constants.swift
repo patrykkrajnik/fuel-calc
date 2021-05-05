c9ef0746db068e67fcd4050a062aca5d30d31849
patrykkrajnik <patrykkrajnik@icloud.com>
2021-05-05 19:10:18 +0200

    Struct with constants created

--- FuelCalc-UIKit/FuelCalc-UIKit/Constants.swift
+++ FuelCalc-UIKit/FuelCalc-UIKit/Constants.swift
@@ -8,5 +8,16 @@
 import Foundation
 
 struct K {
+    static let appName = "FuelCalc"
+    static let calculateButton = "Calculate"
     
+    struct InsertViewTitles {
+        static let distance = "Distance (km)"
+        static let averageConsumption = "Average Consumption (l/100km)"
+        static let fuelPrice = "Price of 1 liter of fuel"
+    }
+    
+    struct InsertViewPlaceholders {
+        static let distance = "eg. 250"
+    }
 }
