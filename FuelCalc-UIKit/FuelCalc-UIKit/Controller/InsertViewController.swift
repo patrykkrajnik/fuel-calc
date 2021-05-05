f47e49cabd80a46985e7fc0ed77c33b60259ec5f
patrykkrajnik <patrykkrajnik@icloud.com>
2021-05-05 19:12:28 +0200

    Basic handlers for user actions created

--- FuelCalc-UIKit/FuelCalc-UIKit/Controller/InsertViewController.swift
+++ FuelCalc-UIKit/FuelCalc-UIKit/Controller/InsertViewController.swift
@@ -8,11 +8,44 @@
 import UIKit
 
 class InsertViewController: UIViewController {
+    
+    let insertView = InsertView()
+    
+    override func loadView() {
+        view = insertView
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        handleUserActions()
+    }
+    
+    @objc func dismissKeyboard() {
+        insertView.endEditing(true)
     }
 
+    @objc func calculateButtonPressed(_ sender: UIButton) {
+        print("Distance: \(insertView.distanceSectionView.textField.text)")
+        print("Fuel consumption: \(insertView.averageConsumptionSectionView.slider.value)")
+        print("Fuel price: \(insertView.fuelPriceSectionView.slider.value)")
+    }
+    
+    @objc func consumptionValueChanged(_ sender: UISlider) {
+        insertView.averageConsumptionSectionView.currentValue.text = String(format: "%.1f", sender.value)
+    }
+    
+    @objc func priceValueChanged(_ sender: UISlider) {
+        insertView.fuelPriceSectionView.currentValue.text = String(format: "%.1f", sender.value)
+    }
+    
+    func handleUserActions() {
+        let tap = UITapGestureRecognizer(target: self, action: #selector(UIInputViewController.dismissKeyboard))
+        insertView.addGestureRecognizer(tap)
+        
+        insertView.calculateButton.addTarget(self, action: #selector(calculateButtonPressed), for: .touchUpInside)
+        insertView.averageConsumptionSectionView.slider.addTarget(self, action: #selector(consumptionValueChanged(_:)), for: .valueChanged)
+        insertView.fuelPriceSectionView.slider.addTarget(self, action: #selector(priceValueChanged(_:)), for: .valueChanged)
+    }
 }
 
