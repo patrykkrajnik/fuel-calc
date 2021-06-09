183f2bd03153614ba769c6b411c663b512b7d5d6
patrykkrajnik <patrykkrajnik@icloud.com>
2021-06-09 14:36:07 +0200

    Init of the project

---
+++ FuelCalc-SwiftUI/FuelCalc-SwiftUI/FuelCalc_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  FuelCalc_SwiftUIApp.swift
+//  FuelCalc-SwiftUI
+//
+//  Created by Patryk Krajnik on 09/06/2021.
+//
+
+import SwiftUI
+
+@main
+struct FuelCalc_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
