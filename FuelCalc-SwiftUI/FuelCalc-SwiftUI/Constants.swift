f14b409a3e31bca51bdfc564317b6ffe621a52d0
patrykkrajnik <patrykkrajnik@icloud.com>
2021-06-09 15:40:08 +0200

    Struct with constants added

---
+++ FuelCalc-SwiftUI/FuelCalc-SwiftUI/Constants.swift
@@ -0,0 +1,14 @@
+//
+//  Constants.swift
+//  FuelCalc-SwiftUI
+//
+//  Created by Patryk Krajnik on 09/06/2021.
+//
+
+import Foundation
+
+struct K {
+    static let appName = "FuelCalc"
+    static let calculateButton = "Calculate"
+    static let recalculateButton = "Recalculate"
+}
