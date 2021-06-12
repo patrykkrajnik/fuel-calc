71518745a20201f0692f58e4a08a5445d78f1a65
patrykkrajnik <patrykkrajnik@icloud.com>
2021-06-12 15:09:50 +0200

    InsertView UI design finished

--- FuelCalc-SwiftUI/FuelCalc-SwiftUI/InsertView.swift
+++ FuelCalc-SwiftUI/FuelCalc-SwiftUI/InsertView.swift
@@ -20,6 +20,16 @@ struct InsertView: View {
                     .fontWeight(/*@START_MENU_TOKEN@*/.bold/*@END_MENU_TOKEN@*/)
                     .position(x: 80.0, y: 20.0)
                     .padding()
+                    .frame(height: 60)
+                InsertSectionTextField(
+                    title: K.InsertViewTitles.distance)
+                InsertSectionSlider(
+                    title: K.InsertViewTitles.averageConsumption,
+                    maximumValue: 40.0)
+                InsertSectionSlider(
+                    title: K.InsertViewTitles.fuelPrice,
+                    maximumValue: 10.0)
+                Spacer()
                 Button(action: {
                     self.showResults = true
                 }) {
@@ -28,26 +38,95 @@ struct InsertView: View {
                             .frame(minWidth: 0, maxWidth: .infinity)
                             .frame(height: 60)
                             .cornerRadius(25)
-                            .padding(.horizontal, 30)
                         Text(K.calculateButton)
-                            .font(.title)
+                            .font(.system(size: 24))
                             .fontWeight(.regular)
                             .foregroundColor(Color.black)
                     }
-                    .padding(20)
                 }
                 .popover(isPresented: $showResults) {
                     ResultView()
                 }
+                .padding(.horizontal, 45)
+                .padding(.bottom, 10)
             }
         }
     }
 }
 
-struct ResultView: View {
+struct InsertSectionTextField: View {
+    
+    @State private var username: String = ""
+    @State private var isEditing = false
+    
+    var title: String
+    
     var body: some View {
-        Text("Result")
-            .padding()
+        ZStack {
+            Color(.systemGray6)
+                .cornerRadius(25.0)
+            VStack(alignment: .leading) {
+                Text(title)
+                    .fontWeight(.regular)
+                    .font(.system(size: 20))
+                    .padding(.horizontal, 15)
+                TextField(
+                    "",
+                    text: $username
+                ) { isEditing in
+                    self.isEditing = isEditing
+                }
+                .keyboardType(.numberPad)
+                .frame(height: 40)
+                .background(Color.white)
+                .padding(.horizontal, 10)
+            }
+        }
+        .frame(height: 140)
+        .padding(.horizontal, 20)
+        .padding(.vertical, 10)
+    }
+}
+
+struct InsertSectionSlider: View {
+    
+    @State private var currentValue: Float = 0.0
+    @State private var isEditing = false
+    
+    var title: String
+    var maximumValue: Float
+    
+    var body: some View {
+        ZStack {
+            Color(.systemGray6)
+                .cornerRadius(25.0)
+            VStack(alignment: .leading) {
+                Text(title)
+                    .fontWeight(.regular)
+                    .font(.system(size: 20))
+                    .padding(.horizontal, 15)
+                Text("\(currentValue.roundToOnePlace(), specifier: "%.1f")")
+                    .fontWeight(.semibold)
+                    .font(.system(size: 22))
+                    .frame(
+                        minWidth: 0,
+                        maxWidth: .infinity,
+                        alignment: .center)
+                    .padding(.vertical, 10)
+                Slider(
+                    value: $currentValue,
+                    in: 0...maximumValue,
+                    onEditingChanged: { editing in
+                        isEditing = editing
+                    }
+                )
+                .accentColor(.gray)
+                .padding(.horizontal, 10)
+            }
+        }
+        .frame(height: 150)
+        .padding(.horizontal, 20)
+        .padding(.vertical, 10)
     }
 }
 
