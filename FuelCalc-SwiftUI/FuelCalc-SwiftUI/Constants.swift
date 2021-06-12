8b8be41bf5f22935c65e0e586e4287f7c9a907a4
patrykkrajnik <patrykkrajnik@icloud.com>
2021-06-12 15:09:26 +0200

    Constants struct extended

--- FuelCalc-SwiftUI/FuelCalc-SwiftUI/Constants.swift
+++ FuelCalc-SwiftUI/FuelCalc-SwiftUI/Constants.swift
@@ -11,4 +11,10 @@ struct K {
     static let appName = "FuelCalc"
     static let calculateButton = "Calculate"
     static let recalculateButton = "Recalculate"
+    
+    struct InsertViewTitles {
+        static let distance = "Distance (km)"
+        static let averageConsumption = "Average Consumption (l/100km)"
+        static let fuelPrice = "Price of 1 liter of fuel"
+    }
 }
