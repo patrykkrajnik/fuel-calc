af97c47b5431ed3ede30fec3004d653059b8d64d
patrykkrajnik <patrykkrajnik@icloud.com>
2021-06-12 15:07:59 +0200

    ResultView created

---
+++ FuelCalc-SwiftUI/FuelCalc-SwiftUI/ResultView.swift
@@ -0,0 +1,21 @@
+//
+//  ResultView.swift
+//  FuelCalc-SwiftUI
+//
+//  Created by Patryk Krajnik on 12/06/2021.
+//
+
+import SwiftUI
+
+struct ResultView: View {
+    var body: some View {
+        Text("Result")
+            .padding()
+    }
+}
+
+struct ResultView_Previews: PreviewProvider {
+    static var previews: some View {
+        ResultView()
+    }
+}
