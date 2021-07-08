95c387a9de80d4c49f1c64d7682a3c59d2b7ed5f
patrykkrajnik <patrykkrajnik@icloud.com>
2021-07-08 19:25:37 +0200

    Orientation locked only to portraitUp

--- fuel_calc_flutter/lib/main.dart
+++ fuel_calc_flutter/lib/main.dart
@@ -1,11 +1,17 @@
 import 'package:flutter/material.dart';
 import 'screens/insert_page.dart';
+import 'package:flutter/services.dart';
 
 void main() => runApp(FuelCalc());
 
 class FuelCalc extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+
+    SystemChrome.setPreferredOrientations([
+      DeviceOrientation.portraitUp
+    ]);
+
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: InsertPage(),
